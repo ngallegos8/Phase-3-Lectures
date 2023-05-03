3946863d9668c636c25f03b11f0a87912d7ecaf0
stephenlambertfs <stephenolambert@gmail.com>
2023-05-03 10:04:25 -0600

    added starter code

---
+++ 03_oop/README.md
@@ -0,0 +1,11 @@
+# Object Oriented Programming in Python
+
+## Learning Goals
+- Object Oriented Programming
+- Benefits and Principles of OOP Design
+- Classes
+- Instances
+- Initializing with attributes using init
+- Instance methods
+- Self
+- Stretch Topics: Object properties
---
+++ 03_oop/lib/pet.py
@@ -0,0 +1,39 @@
+#Todo 1: Create a dog class, first use pass, and create an instance of the class
+#Then use the __init__ to set attributes
+
+    #Global Variables
+
+    #__init__ will set the state when the class is created
+
+
+    #Instance Methods
+
+
+    #Properties
+
+
+    #Class Methods
+
+
+    #__repr__
+
+#Todo 2: Instantiate a few dogs
+
+#Todo 3: Creating a Global Variables
+
+#Todo 4: Create a method and print result
+
+#Todo 5: Output of the __repr__
+
+#Todo 6: use the property() method, to set and get 
+
+#Todo 7: Decorators and CLS (@classmethod)
+
+
+
+
+
+
+
+
+
