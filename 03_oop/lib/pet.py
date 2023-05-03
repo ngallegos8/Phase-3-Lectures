32f8348810a8f6b2b29bcf3fedf2398fcea098a4
stephenlambertfs <stephenolambert@gmail.com>
2023-05-03 11:08:02 -0600

    added starter files

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -1,25 +1,41 @@
 #Todo 1: Create a dog class, first use pass, and create an instance of the class
-#Then use the __init__ to set attributes
+#Then use the __init__ to set attributes, and create an instance of the class
 
     #Global Variables
 
     #__init__ will set the state when the class is created
 
-
+        #append the species to the list, if the species is not in the list
+       
     #Instance Methods
+    #create a method to print the name
+   
 
-
+    #create a method that will add to the number of cupcakes, only if the type is int and cupcakes is greater than 0
+    #if cupcakes is less than 0, raise an error
+    #if cupcakes is not a number, raise an error
+ 
     #Properties
+    #create a property that will get and set the name
+   
+    #To avoid calling get_name and set_name everytime we could use the python decorator property():https://www.programiz.com/python-programming/property 
+    #This will set some code to a attribute of your class
 
 
     #Class Methods
+    #create a class method that will print all the dogs
+   
+    #create a class method that will add a new species to the list
+ 
 
-
-    #__repr__
+    #__repr__ will run when you print on the class
 
 #Todo 2: Instantiate a few dogs
 
+#append a name to the list and print
+
 #Todo 3: Creating a Global Variables
+#create a new array of medicine and append a medicine to the list, print the results
 
 #Todo 4: Create a method and print result
 
@@ -28,7 +44,6 @@
 #Todo 6: use the property() method, to set and get 
 
 #Todo 7: Decorators and CLS (@classmethod)
-
 
 
 
