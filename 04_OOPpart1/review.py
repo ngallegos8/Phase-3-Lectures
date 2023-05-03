38e1fa84d1c659e276ad2d378949ccafe7763795
Stephen Lambert <97977357+l-stephen@users.noreply.github.com>
2023-05-03 16:24:12 -0600

    Delete 04_OOPpart1 directory

--- 04_OOPpart1/review.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-#run chmod +x /path/to/file to create an executable
-# Create a class
-# Create a method
-# Global variables
-# Properties
-# Decorators
-# Review property underscore
