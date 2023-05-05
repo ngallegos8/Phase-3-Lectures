157cc5bbdf350394ae9ed57ad967a9a9e78c8f44
stephenlambertfs <stephenolambert@gmail.com>
2023-05-05 11:50:38 -0600

    updated

---
+++ 05_sql/review.py
@@ -0,0 +1,19 @@
+# One to One
+#Create a one to one relationship between apartments and tennets
+#Apartments should be initialized with name, location, and a tenet that is set to none
+#Create Methods get_tenent that gets the tenent, and set_tenent to set a new tenent
+
+
+
+#One to Many
+#Create a one to many relationship between a Team and Player
+#Team should be initialized with a name, mascot, and have an attrivute players that is a list
+#Player should be initilized with a name
+#Create a method add_player to add a player to the players list
+
+
+#Many to Many
+#Create a many to many relation ship between a teacher and student
+#Teacher should be initialized with name, subject, and have a list of students
+#Student should be initialized with a name, grade, and have a list of teachers
+
