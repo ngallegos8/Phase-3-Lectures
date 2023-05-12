d2549b4583e34c0ddcdf22d3048088491a86aaae
stephenlambertfs <stephenolambert@gmail.com>
2023-05-12 11:00:31 -0600

    edited review.py

---
+++ 08_sqlalchemy2/review.py
@@ -0,0 +1,27 @@
+#Import sqlalchemy
+
+#initialize declarative base
+
+#create a meal class and inheret from base
+
+
+    #create properties
+
+#create a ingredients class and inheret from base
+
+#create a method that creates your tables
+
+
+#create a method to add a meal
+
+
+#create a method to get all of the data
+
+
+#create a method to update the database
+
+
+#create a method to delete the data
+
+
+#if name is main create tables, create meals, and test methods
