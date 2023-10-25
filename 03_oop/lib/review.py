a4f399e9348994c6e52359c1fcbdf7b99745f7ed
stephenlambertfs <stephenolambert@gmail.com>
2023-10-25 10:15:25 -0600

    update starter code

--- 03_oop/lib/review.py
+++ 03_oop/lib/review.py
@@ -1,62 +1,17 @@
-#import dog class from the pet file
-
-#create an instance of the dog class
-
-pet_info = [
-    {
-        'name':'Spot',
-        'age':25,
-        'breed': 'boxer',
-    },
-    {
-        'name':'Rose',
-        'age':11,
-        'breed': 'domestic long-haired',
-    },
-    {
-        'name':'Meow Meow Beans',
-        'age':2,
-        'breed': 'domestic long-haired',
-    }
-]
-
-pet_info_2 = [
-    {
-        'name':'Bud',
-        'age':5,
-        'breed': 'golden retriever',
-    }, 
-    {
-        'name':'Princess Grace',
-        'age':9,
-        'breed': 'persian',
-    },
-    {
-        'name':'WiFi',
-        'age':4,
-        'breed': 'wolf',
-    }
-]
-
-# List Comprehensions => [expression for item in iterable if condition == True]
-#Shorter syntax to create a new list based on the values of an existing list
-# map like 
-#Use list comprehension to return a list containing every pet name from "pet_info" changed to uppercase
-
-# find like
-#Use list comprehension to find a pet named spot
-
-# filter like
-#Use list comprehension to find all of the pets under 3 years old
-
-# Generator Expression => (expression for item in iterable)
-# A function that returns an iterator that produces a sequence of values when iterated over
-# We can make a generator expression by putting list comprehension in parenthesis
-# Or create a generator object with the yield keyword
-# Main Benefit => Less Memory Intensive
-
-#Create a generator expression and generator matching the filter above
+#lambda functions are short annoyomous functions
 
 
+# You can pass a lambda function to a higher order function
+
+
+#raising an error
+# val = False
+# if val == True:
+#     print('Its True')
+# else:
+#     raise Exception("Not True")
+
+#everything in python is an object
+# when you create something python will allocate memory and assign an id
 
 
