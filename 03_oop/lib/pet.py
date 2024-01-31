fb8cc29a6e67c0bedd65458b11672f31e7eac796
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-31 11:27:44 -0700

    lecture complete

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -23,50 +23,127 @@
 
 #Todo 1: Create a dog class, first use pass, and create an instance of the class
 #Then use the __init__ to set attributes, and create an instance of the class
-
+class Dog:
     #Global Variables
-
+    dog_names = []
+    species_list = []
     #__init__ will set the state when the class is created
+    def __init__(self, species, name):
+        self.name = name
+        self.species = species
+        self.food = []
+        self.treats = 0
+        self.dog_names.append(name)
+
+# tracker = Dog("Golden", "Tracker")
+# # instance
+# print(tracker.species)
+# # class
+# print(Dog.dog_names)
 
         #append the species to the list, if the species is not in the list
+        if species not in self.species_list:
+            self.species_list.append(name)
        
     #Instance Methods
     #create a method to print the name
+    def print_name(self):
+        print(self.name)
+    # OR ...
+    def print_name(self):
+        print(f"Hi, {self.name}")
    
 
     #create a method that will add to the number of cupcakes, only if the type is int and cupcakes is greater than 0
     #if cupcakes is less than 0, raise an error
     #if cupcakes is not a number, raise an error
+    def eat_treats(self, num_treats):
+        if type(num_treats) is int and num_treats > 0:
+            self.treats += num_treats
+        elif num_treats < 0:
+            raise ValueError("Cannot be a negative number")
+        else:
+            raise TypeError("Not a number")
  
     #Properties
     #create a property that will get and set the name
-   
+    # def get_name(self):
+    #     print("Getting the name")
+    #     return self._name
+    # def set_name(self, new_name):
+    #     print(f"Setting the name to {new_name}")
+    #     self._name == new_name
+
+    # name = property(get_name, set_name)
+
     #To avoid calling get_name and set_name everytime we could use the python decorator property():https://www.programiz.com/python-programming/property 
     #This will set some code to a attribute of your class
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
 
     #Class Methods
     #create a class method that will print all the dogs
+    @classmethod
+    def print_all_dogs(cls):
+        print(cls.dog_names)
+
    
     #create a class method that will add a new species to the list
+    @classmethod
+    def add_new_species(cls, new_species):
+        cls.species_list.append(new_species)
  
 
     #__repr__ will run when you print on the class
+    def __repr__(self):
+        return (f"Name: {self.name}, Species: {self.species}")
+    
+if __name__ == "__main__":
+
 
 #Todo 2: Instantiate a few dogs
+    tracker = Dog("Golden Lab", "Tracker")
+    boba = Dog("Golden Retriver", "Boba")
 
 #append a name to the list and print
+    boba.dog_names.append("Bobo")
+    print(boba)
+    print(Dog.dog_names)
 
 #Todo 3: Creating a Global Variables
 #create a new array of medicine and append a medicine to the list, print the results
-
+    tracker.medicine = []
+    tracker.medicine.append("Advil")
+    print(tracker.medicine)
+    print(tracker)
+    print(tracker.print_name())
+    tracker.eat_treats(10)
+    print(tracker.treats)
+   
 #Todo 4: Create a method and print result
 
 #Todo 5: Output of the __repr__
 
 #Todo 6: use the property() method, to set and get 
+    boba.name = "Buster"
+    print(boba)
+    print(boba.name)
 
 #Todo 7: Decorators and CLS (@classmethod)
+    Dog.print_all_dogs()
+
+    Dog.add_new_species("Lab")
+    print(Dog.species_list)
+
+    tracker.add_new_species("Terrier")
+    print(Dog.species_list)
 
 
 
--- 03_oop/lib/review.py
+++ 03_oop/lib/review.py
@@ -1,7 +1,17 @@
 #lambda functions are short annoyomous functions
+add = lambda a, b: print(a + b)
+add(10, 20)
 
+# Vanilla version
+def add(a, b):
+    print(a + b)
 
 # You can pass a lambda function to a higher order function
+def multiply(num):
+    return lambda n: num * n
+
+double = multiply(10)
+print(double(10))
 
 
 #raising an error
@@ -11,7 +21,24 @@
 # else:
 #     raise Exception("Not True")
 
+# val = False
+# if val == True:
+#     print("True")
+# else:
+#     raise Exception("False")
+
+# try:
+#     console.log("")
+# except SyntaxError:
+#     print("console is not defined")
+
+
 #everything in python is an object
 # when you create something python will allocate memory and assign an id
 
+x = []
+y = []
+print(x == y)
+print(id(type(x)))
+print(id(list))
 
