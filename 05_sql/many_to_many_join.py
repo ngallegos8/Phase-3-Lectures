f4521dcc431956f1c9cf0ff87a5f4515d54ab9ed
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-02-02 12:04:01 -0700

    lecture complete

--- 05_sql/many_to_many_join.py
+++ 05_sql/many_to_many_join.py
@@ -1,2 +1,51 @@
 #Create a many to many relationship with a join for a sponsor, player, and contract object
 
+class Sponsor:
+    all = []
+    def __init__(self, name, product):
+        self.name = name
+        self.product = product
+        Sponsor.all.append(self)
+
+    def get_contracts(self):
+        for contract in Contract.all:
+            if contract.sponsor is self:
+                print(f"Player: {contract.player.name} Payout: {contract.money}")
+
+class Player:
+    all = []
+
+    def __init__(self, name, team):
+        self.name = name
+        self.team = team
+        Player.all.append(self)
+
+
+class Contract:
+    all = []
+
+    def __init__(self, money, length, sponsor):
+        self.money = money
+        self.length = length
+        self.sponsor = sponsor
+        self.player = None
+        Contract.all.append(self)
+
+    def sign_contract(self, player):
+        if isinstance(player, Player):
+            self.player = player
+
+    def delete(self):
+        del self
+
+nike = Sponsor("Nike", "Clothing")
+jordan = Player("Jordan", "Bulls")
+nike_contract = Contract("1 Billion", "Forever", nike)
+
+nike_contract.sign_contract(jordan)
+nike.get_contracts()
+
+nike_contract.delete()
+print(nike_contract.delete())
+
+print(nike_contract)
--- 05_sql/review.py
+++ 05_sql/review.py
@@ -2,6 +2,37 @@
 #Create a one to one relationship between apartments and tennets
 #Apartments should be initialized with name, location, and a tenet that is set to none
 #Create Methods get_tenent that gets the tenent, and set_tenent to set a new tenent
+class Apartment:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        self.tenet = None
+    
+    def get_tenet(self):
+        return self._tenet
+    
+    def set_tenet(self, new_tenet):
+        if type(new_tenet) is Tenet:
+        # if isinstance(new_tenet, Tenet):
+            if self._tenet is not None:
+                self._tenet.apartment = None
+                new_tenet.apartment = self
+                self._tenet = new_tenet
+            else:
+                self._tenet = None
+
+class Tenet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.apartment = None
+
+david = Tenet("David", 27)
+sunnyside = Apartment("Sunnyside", "Denver")
+david.apartment = sunnyside
+sunnyside.tenet = david
+print(david.apartment)
+print(sunnyside.tenet)
 
 
 #One to Many
@@ -10,11 +41,54 @@
 #Player should be initilized with a name
 #Create a method add_player to add a player to the players list
 
+class Team:
+    def __init__(self, name, mascot):
+        self.name = name
+        self.mascot = mascot
+        self.players = []
+
+    def add_player(self, new_player):
+        if isinstance(new_player, Player):
+            print(new_player.team)
+            self.players.append(new_player)
+            new_player.team = self
+        else:
+            raise Exception("Must be a Player")
+
+class Player:
+    player_list = []
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+        self.team = None
+        Player.player_list.append(name)
+
+player1 = Player("Guy", "Bank Teller")
+player2 = Player("Other Guy", "Security")
+team1 = Team("Free Guy", "Bear")
+
+team1.add_player(player1)
+print(player1.team.name)
+
 
 #Many to Many
 #Create a many to many relation ship between a teacher and student
 #Teacher should be initialized with name, subject, and have a list of students
 #Student should be initialized with a name, grade, and have a list of teachers
+
+class Teacher:
+    students = []
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+        self.students = []
+        Teacher.students.append(self)
+
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+        self.teachers = []
 
 
 
